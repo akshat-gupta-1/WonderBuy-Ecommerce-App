7a979201d07e8f7e04db6b08922d9631e7ae0092
Akshat Gupta <akshatgupta1408@gmail.com>
2023-10-11 23:41:24 +0530

    Changed server api link

--- client/src/components/auth/axiosInstance.tsx
+++ client/src/components/auth/axiosInstance.tsx
@@ -1,5 +1,5 @@
-import axios from 'axios';
-const BASE_URL = 'http://localhost:5000';
+import axios from "axios";
+const BASE_URL = "https://wonder-buy-api.vercel.app";
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
@@ -8,6 +8,6 @@ export const axiosPrivate = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
