3ca49ec8e1a68a2cc591ab33c597757ee0f350b4
Akshat Gupta <akshatgupta1408@gmail.com>
2023-10-11 23:52:08 +0530

    updated axios Instance

--- client/src/components/auth/axiosInstance.tsx
+++ client/src/components/auth/axiosInstance.tsx
@@ -2,11 +2,9 @@ import axios from "axios";
 const BASE_URL = "https://wonder-buy-api.vercel.app";
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
-  withCredentials: true,
 });
 export const axiosPrivate = axios.create({
   baseURL: BASE_URL,
-  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
